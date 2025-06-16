6c77ba90801e27c43b91f0111092e4597d0d5cc9
gillanihassan <hassangillani397@gmail.com>
2025-06-16 17:14:13 +0500

    heroSection complete update

--- src/assets/components/heroSection/HeroSection.jsx
+++ src/assets/components/heroSection/HeroSection.jsx
@@ -4,19 +4,33 @@ import HeroImage from "../.././../assets/images/heroSection/HeroImage.png";
 
 function HeroSection() {
   return (
-    <div className={`container-xxl ${styles.mainDiv}`}>
+    <div className={`container-xxl   ${styles.mainDiv}`}>
       <Row>
-        <Col md={7}>
-          <h2>Unlock Your True Potential For Free!</h2>
-          <p>
+        <Col md={7} className="d-flex flex-column justify-content-center ">
+          <h2 className={` ${styles.topHeading}`}>
+            Unlock Your True Potential For Free!
+          </h2>
+          <p className={`mt-1  ${styles.customPara}`}>
             Discover scientifically-backed tools to boost your personal growth,
             completely free of charge.
           </p>
-          <Button variant="success">Join For Free</Button>
-          <Button variant="warning">Already Have an Account</Button>
+          <div className={`mt-4 ${styles.buttonDiv}`}>
+            <Row className="g-2">
+              <Col xs={12} md="auto">
+                <Button className={` px-4 ${styles.btnOne}`}>
+                  Join For Free
+                </Button>
+              </Col>
+              <Col xs={12} md="auto">
+                <Button className={` px-3 ${styles.btnTwo}`}>
+                  Already Have an Account
+                </Button>
+              </Col>
+            </Row>
+          </div>
         </Col>
-        <Col md={5}>
-          <Image src={HeroImage} />
+        <Col md={5} className="text-end text-md-center">
+          <Image src={HeroImage} className={`${styles.customImage}`} />
         </Col>
       </Row>
     </div>
