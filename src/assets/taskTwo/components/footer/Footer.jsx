a6fcac11c636e1bb0546e238525a3ab378a2f78e
gillanihassan <hassangillani397@gmail.com>
2025-06-18 00:39:20 +0500

    footer section complete

--- src/assets/taskTwo/components/footer/Footer.jsx
+++ src/assets/taskTwo/components/footer/Footer.jsx
@@ -1,153 +1,111 @@
 import React from "react";
-import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import {
+  Container,
+  Row,
+  Col,
+  Form,
+  Button,
+  Image,
+  InputGroup,
+} from "react-bootstrap";
+import styles from "./styles.module.css";
 import FooterLogo from "../../../taskTwo/images/footer/footer-logo.png";
 import facebook from "../../../taskTwo/images/footer/facebook.png";
 import instagram from "../../../taskTwo/images/footer/instagram.png";
 import twitter from "../../../taskTwo/images/footer/twitter.png";
-// import phoneIcon from "./phone.png";
-// import emailIcon from "./email.png";
+import Mail from "../../../taskTwo/images/footer/mail.svg";
+import Phone from "../../../taskTwo/images/footer/phone.svg";
 
 const Footer = () => {
   return (
-    <footer
-      style={{
-        backgroundColor: "#F5F5F5",
-        padding: "20px 0",
-        color: "#1E3A8A",
-        border: "4px solid red",
-      }}
-    >
-      <Container>
+    <footer className={`${styles.footerDiv}`}>
+      <Container className="p-4">
         <Row>
-          {/* Logo Column */}
           <Col md={3}>
-            <img
+            <Image
               src={FooterLogo}
               alt="Bugelo Logo"
-              style={{ width: "100px" }}
+              className={`${styles.footerLogo}`}
             />
-            <p style={{ marginTop: "10px" }}>
+            <p className={`mt-4 ${styles.footerParaOne}`}>
               We Help Fast Growing Brands Get Amazon Under Control.
             </p>
-            <div style={{ marginTop: "10px" }}>
-              <img
+            <div className="mt-4">
+              <Image
                 src={facebook}
                 alt="Facebook"
-                style={{ width: "24px", marginRight: "10px" }}
+                className={`${styles.footerIcons}`}
               />
-              <img
+              <Image
                 src={instagram}
                 alt="Instagram"
-                style={{ width: "24px", marginRight: "10px" }}
+                className={`mx-3 ${styles.footerIcons}`}
               />
-              <img src={twitter} alt="Twitter" style={{ width: "24px" }} />
+              <Image
+                src={twitter}
+                alt="Twitter"
+                className={`${styles.footerIcons}`}
+              />
             </div>
           </Col>
 
-          {/* Quick Links Column */}
           <Col md={3}>
-            <h5>Quick Link</h5>
+            <h5 className={`${styles.footerHeading}`}>Quick Link</h5>
             <ul style={{ listStyle: "none", padding: 0 }}>
-              <li>
-                <a
-                  href="#home"
-                  style={{ color: "#1E3A8A", textDecoration: "none" }}
-                >
-                  Home
-                </a>
+              <li className="mt-4 mb-2">
+                <a className={` ${styles.footerLink}`}>Home</a>
+              </li>
+              <li className="mb-2">
+                <a className={`${styles.footerLink}`}>About Us</a>
+              </li>
+              <li className="mb-2">
+                <a className={`${styles.footerLink}`}>What We Do</a>
+              </li>
+              <li className="mb-2">
+                <a className={`${styles.footerLink}`}>Our Process</a>
+              </li>
+              <li className="mb-2">
+                <a className={`${styles.footerLink}`}>Team</a>
+              </li>
+              <li className="mb-2">
+                <a className={`${styles.footerLink}`}>About</a>
               </li>
               <li>
-                <a
-                  href="#about-us"
-                  style={{ color: "#1E3A8A", textDecoration: "none" }}
-                >
-                  About Us
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#what-we-do"
-                  style={{ color: "#1E3A8A", textDecoration: "none" }}
-                >
-                  What We Do
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#our-process"
-                  style={{ color: "#1E3A8A", textDecoration: "none" }}
-                >
-                  Our Process
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#team"
-                  style={{ color: "#1E3A8A", textDecoration: "none" }}
-                >
-                  Team
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#about"
-                  style={{ color: "#1E3A8A", textDecoration: "none" }}
-                >
-                  About
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#contact"
-                  style={{ color: "#1E3A8A", textDecoration: "none" }}
-                >
-                  Contact
-                </a>
+                <a className={`${styles.footerLink}`}>Contact</a>
               </li>
             </ul>
           </Col>
 
           {/* Contact Information Column */}
           <Col md={3}>
-            <h5>Contact Information</h5>
-            <p>
-              {/* <img src={phoneIcon} alt="Phone" style={{ width: "16px", marginRight: "5px" }} /> */}
+            <h5 className={`${styles.footerHeading}`}>Contact Information</h5>
+            <p className={`mt-5 ${styles.footerLink}`}>
+              <Image src={Phone} alt="Phone" className="mx-2" />
               +420 731 255 465
             </p>
-            <p>
-              {/* <img src={emailIcon} alt="Email" style={{ width: "16px", marginRight: "5px" }} /> */}
+            <p className={` ${styles.footerLink}`}>
+              <Image src={Mail} alt="Email" className="mx-2" />
               ales.kylar@bugelo.com
             </p>
           </Col>
 
           {/* Newsletter Subscription Column */}
           <Col md={3}>
-            <h5>Subscribe Our Newsletter</h5>
-            <Form>
-              <Form.Control
-                type="email"
-                placeholder="Enter Email"
-                style={{ marginBottom: "10px" }}
-              />
-              <Button
-                variant="info"
-                style={{
-                  backgroundColor: "#00C4CC",
-                  border: "none",
-                  width: "100%",
-                }}
-              >
-                SUBSCRIBE
-              </Button>
+            <h5 className={`${styles.footerHeading}`}>
+              Subscribe Our Newsletter
+            </h5>
+            <Form className="mt-4">
+              <InputGroup>
+                <Form.Control
+                  type="email"
+                  placeholder="enter email"
+                  className={`${styles.formControl}`}
+                />
+                <InputGroup.Text className={`${styles.inputGroup}`}>
+                  SUBSCRIBE
+                </InputGroup.Text>
+              </InputGroup>
             </Form>
-          </Col>
-        </Row>
-        <Row>
-          <Col
-            className="text-center"
-            style={{ marginTop: "20px", color: "#1E3A8A" }}
-          >
-            <p>©2022 Bugelo.com all right reserved.</p>
           </Col>
         </Row>
       </Container>
