e42beac5c30164e436d6061cfbcb47d5e09737f3
gillanihassan <hassangillani397@gmail.com>
2025-06-18 00:42:35 +0500

    footer section

--- src/assets/taskTwo/components/amazonGrowth/AmazonGrowth.jsx
+++ src/assets/taskTwo/components/amazonGrowth/AmazonGrowth.jsx
@@ -13,9 +13,7 @@ function AmazonGrowth() {
           <p className={`text-uppercase mb-1 ${styles.topSpan}`}>
             pan european
           </p>
-          <h2 className={`${styles.topHeading}`}>
-            Pan European Amazon Experts
-          </h2>
+          <h2 className={`${styles.topHeading}`}>Pan European Amazon Expert</h2>
           <p className={`text-center mt-1 mt-sm-3 ${styles.mainPara}`}>
             By focusing only on Amazon Marketplace, we can stay up to date on
             the latest trends, monitor and test new strategies to maximise your
