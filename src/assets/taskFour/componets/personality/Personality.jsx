b19010b67a3c3cff891f270996c09dd70e604be8
gillanihassan <hassangillani397@gmail.com>
2025-06-19 23:45:05 +0500

    footer update

--- src/assets/taskFour/componets/personality/Personality.jsx
+++ src/assets/taskFour/componets/personality/Personality.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import MainSection from "./mainSection/MainSection";
+import Footer from "../../componets/four/footer/FooterSection";
 
 function Personality() {
   return (
     <div>
       <MainSection />
+      <Footer />
     </div>
   );
 }
