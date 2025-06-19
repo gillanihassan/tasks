bace08bea6272f01acac19010dc75d41f8e55dab
gillanihassan <hassangillani397@gmail.com>
2025-06-19 11:22:02 +0500

    task three navbar update

--- src/assets/taskThree/components/about/AboutPage.jsx
+++ src/assets/taskThree/components/about/AboutPage.jsx
@@ -1,11 +1,15 @@
+import LicenseNav from "../license/navbar/LicenseNav";
 import Company from "./company/Company";
 import Team from "./team/Team";
+import LicenseFooter from "../license/footer/LicenseFooter";
 
 function AboutPage() {
   return (
     <div>
+      <LicenseNav />
       <Company />
       <Team />
+      <LicenseFooter />
     </div>
   );
 }
