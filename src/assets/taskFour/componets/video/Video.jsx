909c127484b6ceb010d774c73347f432ea6c861e
gillanihassan <hassangillani397@gmail.com>
2025-06-19 22:53:44 +0500

    add footer

--- src/assets/taskFour/componets/video/Video.jsx
+++ src/assets/taskFour/componets/video/Video.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import TopSection from "./topSection/TopSection";
+import Footer from "../../componets/four/footer/FooterSection";
 
 function Video() {
   return (
     <div>
       <TopSection />
+      <Footer />
     </div>
   );
 }
